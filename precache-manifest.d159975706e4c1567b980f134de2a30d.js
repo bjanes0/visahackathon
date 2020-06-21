01c118e3f62cc58b24b89d1cbc53cdbbe98187cf
Nasuru <nasuruf@gmail.com>
2020-06-21 16:03:13 -0600

    Updates

---
+++ precache-manifest.d159975706e4c1567b980f134de2a30d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a34326c030920110fffd4e152d457b",
+    "url": "/visahackathon/index.html"
+  },
+  {
+    "revision": "b4f19d8eefa3a8c84c3d",
+    "url": "/visahackathon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10cad5486d9d2fa69fee",
+    "url": "/visahackathon/static/js/2.c529b2f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/visahackathon/static/js/2.c529b2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f19d8eefa3a8c84c3d",
+    "url": "/visahackathon/static/js/main.ff95e916.chunk.js"
+  },
+  {
+    "revision": "373f0657adccbdb05166",
+    "url": "/visahackathon/static/js/runtime-main.647438cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/visahackathon/static/media/logo.5d5d9eef.svg"
+  }
+]);
