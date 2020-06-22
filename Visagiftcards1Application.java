9caf7a2d08e909d045c71550884a52473776c88a
naserharoon54 <66799558+naserharoon54@users.noreply.github.com>
2020-06-22 16:29:11 -0400

    Add files via upload

---
+++ Visagiftcards1Application.java
@@ -0,0 +1,13 @@
+package com.visa.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Visagiftcards1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Visagiftcards1Application.class, args);
+	}
+
+}
---
+++ gift_giver_controller.java
@@ -0,0 +1,89 @@
+package com.visa.springboot.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.visa.springboot.exception.ResourceNotFoundException;
+import com.visa.springboot.model.gift_givers;
+import com.visa.springboot.repository.gift_givers_repository;
+
+
+@RestController
+@RequestMapping("/api/v1/")
+public class gift_giver_controller {
+
+	@Autowired
+	private gift_givers_repository giftGiverRepository;
+	// get gift givers
+	
+	@GetMapping("gift_givers")
+	public List<gift_givers> getAllGiftGivers(){
+		return this.giftGiverRepository.findAll();
+	}
+	
+	// get gift givers by id
+	  @GetMapping("/gift_givers/{userId}")
+	    public ResponseEntity<gift_givers> getEmployeeById(@PathVariable(value = "userId") Long userId)
+	        throws ResourceNotFoundException {
+	        gift_givers giftGiver = giftGiverRepository.findById(userId)
+	          .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
+	        return ResponseEntity.ok().body(giftGiver);
+	    }	 
+	
+	// save gift giver
+	    
+	    @PostMapping("/gift_givers")
+	    public gift_givers createGiftGiver(@Valid @RequestBody gift_givers giftGiver) {
+	        return giftGiverRepository.save(giftGiver);
+	    }	    
+	    
+	// update gift giver
+	    
+	    @PutMapping("/gift_givers/{userId}")
+	    public ResponseEntity<gift_givers> updateEmployee(@PathVariable(value = "userId") Long userId,
+	         @Valid @RequestBody gift_givers giftGiverDetails) throws ResourceNotFoundException {
+	        gift_givers giftGiver = giftGiverRepository.findById(userId)
+	        .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
+
+	        giftGiver.setGiftCampaignId(giftGiverDetails.getGiftCampaignId());
+	    
+	        final gift_givers updatedGiftGIver = giftGiverRepository.save(giftGiver);
+	        return ResponseEntity.ok(updatedGiftGIver);
+	    }	    
+	    
+	   
+	    
+	// delete gift giver
+	    
+	    @DeleteMapping("/gift_givers/{userId}")
+	    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "userId") Long userId)
+	         throws ResourceNotFoundException {
+	        gift_givers giftGiver = giftGiverRepository.findById(userId)
+	       .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
+
+	        giftGiverRepository.delete(giftGiver);
+	        Map<String, Boolean> response = new HashMap<>();
+	        response.put("deleted", Boolean.TRUE);
+	        return response;
+	    }
+	    
+	    
+	    
+	    
+	    
+	
+}
