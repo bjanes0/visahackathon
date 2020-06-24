0f93ebcdf3550dff740d4710e9d28b0a6c960233
bjanes0 <bjanes@umass.edu>
2020-06-24 14:57:00 -0400

    added basic search

--- visa_proj/src/pages/search.jsx
+++ visa_proj/src/pages/search.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Form, Container, Row, Col, Image } from 'react-bootstrap';
+import '../css/search.css';
+
 
 export const Search = () => (
     <React.Fragment>
-        <h2>Search</h2>
+        <div id="top_div">
+            <h2>Search</h2>
+            <Form>
+                <Form.Group>
+                    <Form.Control type="search" placeholder="Search gifts" />
+                </Form.Group>
+            </Form>
+        </div>
     </React.Fragment >
 )
