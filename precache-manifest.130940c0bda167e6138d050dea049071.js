46782303276874610f63148bf2bbbedda0f8674b
bjanes0 <bjanes@umass.edu>
2020-06-24 21:58:40 -0400

    Updates

---
+++ precache-manifest.130940c0bda167e6138d050dea049071.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0c9daead0ceb47ab52c968c8e55daa",
+    "url": "/visahackathon/index.html"
+  },
+  {
+    "revision": "a0fc23d10f62b0d86955",
+    "url": "/visahackathon/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ed6508c2cfe7ecc18f75",
+    "url": "/visahackathon/static/css/main.6109e4a7.chunk.css"
+  },
+  {
+    "revision": "a0fc23d10f62b0d86955",
+    "url": "/visahackathon/static/js/2.5e1a4935.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/visahackathon/static/js/2.5e1a4935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6508c2cfe7ecc18f75",
+    "url": "/visahackathon/static/js/main.2b452438.chunk.js"
+  },
+  {
+    "revision": "373f0657adccbdb05166",
+    "url": "/visahackathon/static/js/runtime-main.647438cd.js"
+  },
+  {
+    "revision": "463d792e23eda7f1b2a038a074d098e0",
+    "url": "/visahackathon/static/media/pic1.463d792e.jpg"
+  },
+  {
+    "revision": "ae0c07b68b926f1c0052376d4b00832f",
+    "url": "/visahackathon/static/media/pic2.ae0c07b6.png"
+  },
+  {
+    "revision": "aa9337f7355b4df54b2179c69e22b689",
+    "url": "/visahackathon/static/media/pic3.aa9337f7.jpg"
+  },
+  {
+    "revision": "b8be50c7b9283aa0e1172c9de371a986",
+    "url": "/visahackathon/static/media/sampleCard.b8be50c7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/visahackathon/precache-manifest.d159975706e4c1567b980f134de2a30d.js"
+  "/visahackathon/precache-manifest.130940c0bda167e6138d050dea049071.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/visahackathon/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
