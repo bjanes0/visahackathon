d31fb2743e0324b0cd726a9eb2c38f52f01c9d02
Nasuru <nasuruf@gmail.com>
2020-06-29 19:59:56 -0600

    added gift templates page

---
+++ visa_proj/src/pages/giftTemplates.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Card} from 'react-bootstrap';
+
+
+class GiftTemplates extends Component {
+    
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default GiftTemplates;
